890d55c43bf3ae4aff00307cca551683ec7df91a
mnaumanali94 <mnaumanali94@gmail.com>
2016-11-29 17:10:37 +0500

    Added date time converters version 1.0.0.45

---
+++ APIMATIC.SDK.Common/Common/CustomDateTimeConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Converters;
+
+namespace APIMATIC.SDK.Common.Common
+{
+    public class CustomDateTimeConverter : IsoDateTimeConverter
+    {
+        public CustomDateTimeConverter(string format)
+        {
+            DateTimeFormat = format;
+        }
+    }
+}
